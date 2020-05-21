6151379dcab528f078509b4d253104438e62ee48
lijinzhong <lijinzhong@zed.com>
2020-05-21 11:12:53 +0800

    登录

--- src/test/java/com/zhonggg/ZhongggApplicationTests.java
+++ src/test/java/com/zhonggg/ZhongggApplicationTests.java
@@ -2,15 +2,20 @@ package com.zhonggg;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZhongggApplicationTests {
-
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
     @Test
     public void contextLoads() {
+        redisTemplate.opsForValue().set("myKey","myValue");
+        System.out.println(redisTemplate.opsForValue().get("myKey"));
     }
 
 }
